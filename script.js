df3c7ef08080b8b69e86d5784e81f5c794cac41b
Zi-on <zioniflores12@yahoo.com>
2021-05-14 18:00:06 -0500

    added the prompts asking if you want uppercase, lowercase, numerical, and special characters and then asks how long you want the password

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 let lowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numberChoices=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let specialChoices=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "/", "?", "<", ">"]
+let SpecialCharacters=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "/", "?", "<", ">"]
 
 // Write password to the #password input
 function writePassword() {
@@ -14,6 +14,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var useLowerCase = prompt("Do you want uppercase characters", "Y/N");
+  var useUpperCase = prompt("Do you want uppercase characters", "Y/N");
+  var useNumberChoices = prompt("Do you want numerical characters", "Y/N");
+  var useSpecialCharacters = prompt("Do you want special characters", "Y/N");
+  var passwordLength = prompt("How long do you want you password", "password must be larger then 8 and lower then 128");
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
